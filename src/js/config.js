e09596bbf0ab68435b777622fbf0fd33eeee7c51
LAAJILIC <laajili.cyrine@gmail.com>
2021-02-16 00:38:14 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1a5fd3dc-d293-4e1c-8545-fe39945abe9c';
+export const MODAL_CLOSE_SEC = 2.5;
